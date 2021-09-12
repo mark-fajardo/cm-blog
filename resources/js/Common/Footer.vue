e9084ba389eaa5b7d7ef3d1c7b69a364e4850c88
Mark Joshua Fajardo <mjt.fajardo@gmail.com>
2021-09-12 13:23:32 +0800

    fix: minor ui issues

--- resources/js/Common/Footer.vue
+++ resources/js/Common/Footer.vue
@@ -23,7 +23,7 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row below-footer">
                 <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
                     <div class="footer-menu">
                         <ul>
