f808bf473537e0db83680c934242dac27b63698c
Mark Joshua Fajardo <mjt.fajardo@gmail.com>
2021-09-14 00:04:33 +0800

    feat: update g-tag ID

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,13 +27,13 @@
     <script type="text/javascript" src="{{ asset('/js/main.js') }}" defer></script>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1V86N2CL6N"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-207398048-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-1V86N2CL6N');
+        gtag('config', 'UA-207398048-1');
     </script>
 </head>
 <body data-state="{{ $state }}">
