5d100fb97e524eadd4a8849ce094bdf98ce4e0b5
Mark Joshua Fajardo <mjt.fajardo@gmail.com>
2021-11-13 19:50:58 +0800

    chore: update recipe view header image to current recipe main image

--- resources/js/Components/Recipe/Header.vue
+++ resources/js/Components/Recipe/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="top-area single-page single-blog-page" id="home">
-        <div class="page-barner-bg"></div>
+        <div class="page-barner-bg" :style="'background: rgba(0, 0, 0, 0) url(\'' + this.recipe.main_image + '\') no-repeat scroll center center / cover'"></div>
         <Nav/>
         <PageBanner banner-for="recipe"/>
     </header>
@@ -9,9 +9,13 @@
 <script>
     import Nav from '../../Common/Nav';
     import PageBanner from '../../Common/PageBanner';
+    import { mapGetters } from 'vuex';
 
     export default {
         name: 'Header',
-        components: { PageBanner, Nav }
+        components: { PageBanner, Nav },
+        computed: {
+            ...mapGetters('Recipe', ['recipe']),
+        }
     }
 </script>
